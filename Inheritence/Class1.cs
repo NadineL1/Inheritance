39389aa29267dfe341ad3518bf5c202d94cc54ad
NadineL1 <nadine.sorinder@gmail.com>
2025-09-24 11:08:42 +0200

    Add project files.

---
+++ Inheritence/Class1.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritence
+{
+    internal class Animal
+    {
+        // Base class method
+
+        // fields/properties
+        public string Color { get; set; }
+        public string Food { get; set; }
+        public bool hasWings { get; set; }
+        public string SkinType { get; set; }
+        public string Sound { get; set; }
+        public string Pattern { get; set; }
+
+        // constructor for animal class
+        public Animal()
+        { 
+            Color = "Brown";
+            Food = "food";
+            hasWings = false;
+            SkinType = "Skin";
+            Sound = "speaks";
+            Pattern = "plain";
+        }
+
+        // methods for animal class
+        public void Eat()
+        {
+            Console.WriteLine($"This animal eats {Food}");
+        }
+        public void Speak()
+        {
+            Console.WriteLine($"This animal {Sound}");
+        }
+        public void Move()
+        {
+            Console.WriteLine("This animal moves");
+        }
+
+    }
+}
---
+++ Inheritence/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Inheritence
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal animal1 = new();
+            Console.WriteLine($"This animal has skintype: {animal1.SkinType} is color: {animal1.Color} and has {animal1.Food} as it's favourite food.");
+
+            animal1.Eat();
+            animal1.Speak();
+            animal1.Move();
+
+            
+
+        }
+    }
+}
