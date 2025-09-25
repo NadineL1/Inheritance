98b8e6b04a81ac40ed49440514692bf43bac6be6
NadineL1 <nadine.sorinder@gmail.com>
2025-09-25 14:01:22 +0200

    all derived classes in horse has specific property and method

--- Inheritence/HorseClass.cs
+++ Inheritence/HorseClass.cs
@@ -4,24 +4,32 @@ namespace Inheritence
 {
     internal class Horse : Animal
     {
+        public bool IsUsedAsVechicle { get; set; }
         public Horse()
         {
             Color = "tan";
             Food = "hay";
             HasWings = false;
+            IsUsedAsVechicle = true;
             SkinType = "fur";
             Sound = "neighs";
             Pattern = "plain";
             Movement = "gallops";
         }
+        public void JumpingCompetition()
+        {
+            Console.WriteLine("The horse has entered a jumping competition.");
+        }
     }
     internal class Donkey : Horse
     {
+        public bool IsCharacterInShrek { get; set; }
         public Donkey()
         { 
             Color = "gray";
-            Sound = "brays";
+            IsCharacterInShrek = true;
             Movement = "strut";
+            Sound = "brays";   
         }
         public void Kick()
         {
@@ -30,10 +38,12 @@ namespace Inheritence
     }
     internal class Zebra : Horse
     {
+        public bool IsEndangered {  get; set; }
         public Zebra()
         { 
             Color = "black and white";
             Food = "grass";
+            IsEndangered = true;
             Pattern = "striped";
             Sound = "barks";
         }
