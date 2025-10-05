33c52b887b1ddc07c8d2a490b0f3247915540a60
NadineL1 <nadine.sorinder@gmail.com>
2025-10-05 17:28:11 +0200

    Comments birdclass

--- Inheritence/BirdClass.cs
+++ Inheritence/BirdClass.cs
@@ -2,9 +2,12 @@
 
 namespace Inheritence
 {
+    // subclass for birds, inheriting from animal
     internal class BirdClass : Animal
     {
+        // new property for birdclass
         public string Dancing { get; set; }
+        // constructor for birdclass with set defaut values
         public BirdClass()
         {
             Color = "yellow";
@@ -16,6 +19,7 @@ namespace Inheritence
             SkinType = "feathers";
             Sound = "chirps";
         }
+        // methods for bird class
         public void Dance()
         {
             Console.WriteLine($"The birds are doing a {Dancing}.");
@@ -25,9 +29,13 @@ namespace Inheritence
             Console.WriteLine("The bird has laid an egg!");
         }
     }
+    // subclass Chicken
     internal class Chicken : BirdClass
     {
+        // property for chicken class
         public bool CanBePeopleFood { get; set; }
+
+        // constructor for chicken class with default values
         public Chicken()
         {
             CanBePeopleFood = true;
@@ -35,29 +43,37 @@ namespace Inheritence
             Food = "grains";
             Movement = "walks and flaps its wings";
         }
+        // method for chicken class
         public void Peck()
         {
             Console.WriteLine($"The chicken pecks for {Food} the ground.");
         }
     }
+    // subclass flamingo
     internal class Flamingo : BirdClass
     {
+        // property for Flamingo
         public bool MateForLife {  get; set; }
+        // constructor for flamingo w default values
         public Flamingo()
         {
             Color = "pink";
             Food = "crustasceans";
             MateForLife = true;
         }
+        // method for flamingo subclass
         public void StandOnOneLeg()
         {
             Console.WriteLine("The flamingo is standing on one leg.");
         }
     }
+    // subclass Owl
     internal class  Owl : BirdClass
     {
+        // properties for subclass owl
         public bool IsNocturnal { get; set; }
         public bool SucessfulHunt { get; set; }
+        // constructor for Owl w default values
         public Owl()
         {
             Color = "brown";
@@ -66,6 +82,7 @@ namespace Inheritence
             Movement = "turns its head 360 degrees.";
             Sound = "hoots";
         }
+        // method for Owl
         public bool Hunting()
         {
             Console.WriteLine($"The owl is hunting for it's prey, a {Food}.");
